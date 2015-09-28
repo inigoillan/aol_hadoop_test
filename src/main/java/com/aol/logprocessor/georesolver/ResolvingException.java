70aae840dd7634eeecf729997c18a2fd9bfcad5d
Inigo Illan <inigo.illan@gmail.com>
2015-09-28 13:30:37 +0100

    New GeoResolver class which uses GeoIP database

---
+++ src/main/java/com/aol/logprocessor/georesolver/ResolvingException.java
@@ -0,0 +1,9 @@
+package com.aol.logprocessor.georesolver;
+
+/**
+ * @author <a href="mailto:inigo.illan@gmail.com">Inigo Illan</a>
+ * @since 1.0
+ */
+public class ResolvingException extends Exception {
+
+}
---
+++ src/test/java/com/aol/logprocessor/georesolver/GeoResolverTest.java
@@ -0,0 +1,56 @@
+package com.aol.logprocessor.georesolver;
+
+import com.google.common.base.Throwables;
+import org.junit.Test;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import static org.junit.Assert.*;
+
+
+public class GeoResolverTest {
+
+    // IP Address are taken from:
+    // http://www.nirsoft.net/countryip/gb.html
+    @Test
+    public void GetCountry_GivenGBIP_ReturnsUK() throws ResolvingException {
+        // Arrange
+        InetAddress address = getInetAddress("2.24.1.1");
+        GeoResolver resolver = new GeoResolver();
+
+        // Act
+        GeoLocation country = resolver.resolveLocation(address);
+
+        // Assert
+        GeoLocation expectedLocation = buildLocation("GB");
+        assertEquals(expectedLocation, country);
+    }
+
+    @Test(expected = ResolvingException.class)
+    public void GetCountry_GivenInvalidIP_ThrowsResolvingException() throws ResolvingException {
+        // Arrange
+        InetAddress address = getInetAddress("192.168.0.1");
+        GeoResolver resolver = new GeoResolver();
+
+        // Act
+        resolver.resolveLocation(address);
+    }
+
+
+    private GeoLocation buildLocation(String countryCode) {
+        return new GeoLocation(countryCode);
+    }
+
+    private InetAddress getInetAddress(String ip) {
+        try {
+            return Inet4Address.getByName(ip);
+        } catch (UnknownHostException e) {
+            Throwables.propagate(e);
+        }
+
+        // Just to make the compiler happy
+        return null;
+    }
+}
