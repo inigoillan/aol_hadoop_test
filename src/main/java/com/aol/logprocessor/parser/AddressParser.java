d89cb855eaefb33f7bd07376991888e05be67277
Inigo Illan <inigo.illan@gmail.com>
2015-09-28 17:11:31 +0100

    Added some documentation

--- src/main/java/com/aol/logprocessor/parser/AddressParser.java
+++ src/main/java/com/aol/logprocessor/parser/AddressParser.java
@@ -5,7 +5,19 @@ package com.aol.logprocessor.parser;
  * @since 1.0
  */
 public interface AddressParser<T extends Address> {
+    /**
+     * Parses an address. Note that if {@link #canParse(String)} returns true, then this method should never fail
+     *
+     * @param address
+     * @return
+     */
     T parse(String address);
 
+    /**
+     * Checks whether this parser can parse the given address
+     *
+     * @param address
+     * @return
+     */
     boolean canParse(String address);
 }
