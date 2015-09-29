718e4d057e303bf6cdf6b58643fd8aa3c966533b
Inigo Illan <inigo.illan@gmail.com>
2015-09-29 18:01:21 +0100

    Added aggregation key class and tests

---
+++ src/main/java/com/aol/logprocessor/hadoop/datatypes/AggregationKey.java
@@ -0,0 +1,84 @@
+package com.aol.logprocessor.hadoop.datatypes;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+
+import javax.annotation.Nonnull;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * Set of keys use for the aggregation. Take into account that the order of the keys passed into the constructor, matters
+ *
+ * @author <a href="mailto:inigo.illan@gmail.com">Inigo Illan</a>
+ * @since 1.0
+ */
+public class AggregationKey implements WritableComparable<AggregationKey> {
+    String[] keys;
+
+    /**
+     * Don't use this constructor. This is meant for serializing and deserializing purposes
+     */
+    public AggregationKey() {}
+
+    public AggregationKey(@Nonnull String... keys) {
+        this.keys = keys;
+    }
+
+    @Override
+    public int compareTo(AggregationKey aggregationKey) {
+        if (this.keys.length != aggregationKey.keys.length) {
+            return this.keys.length < aggregationKey.keys.length ? -1 : 1;
+        }
+
+        for (int i = 0; i < keys.length; i++) {
+            if (!this.keys[i].equals(aggregationKey.keys[i])) {
+                return this.keys[i].compareTo(aggregationKey.keys[i]);
+            }
+        }
+
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeInt(keys.length);
+
+        Text text = new Text();
+
+        for (int i = 0; i < keys.length; i++) {
+            text.set(keys[i]);
+            text.write(dataOutput);
+        }
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        int size = dataInput.readInt();
+
+        keys = new String[size];
+
+        Text text = new Text();
+
+        for (int i = 0; i < keys.length; i++) {
+            text.readFields(dataInput);
+            keys[i] = text.toString();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || !(o instanceof AggregationKey))
+            return false;
+
+        if (this == o)
+            return true;
+
+        AggregationKey other = (AggregationKey) o;
+
+        return Arrays.equals(this.keys, other.keys);
+    }
+}
---
+++ src/test/java/com/aol/logprocessor/hadoop/datatypes/AggregationKeyTest.java
@@ -0,0 +1,157 @@
+package com.aol.logprocessor.hadoop.datatypes;
+
+import org.junit.Test;
+
+import java.io.*;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author <a href="mailto:inigo.illan@gmail.com">Inigo Illan</a>
+ * @since 1.0
+ */
+public class AggregationKeyTest {
+
+    //region CompareTo tests
+
+    @Test
+    public void CompareTo_EqualKeys_Returns0() {
+        // Arrange
+        AggregationKey aggregationKey1 = new AggregationKey("a", "b");
+        AggregationKey aggregationKey2 = new AggregationKey("a", "b");
+
+        // Act
+        int compareResult = aggregationKey1.compareTo(aggregationKey2);
+
+        // Assert
+        assertEquals(0, compareResult);
+    }
+
+    @Test
+    public void CompareTo_FirstKeysSizeLower_ReturnsMinus1() {
+        // Arrange
+        AggregationKey aggregationKey1 = new AggregationKey("a");
+        AggregationKey aggregationKey2 = new AggregationKey("a", "b");
+
+        // Act
+        int compareResult = aggregationKey1.compareTo(aggregationKey2);
+
+        // Assert
+        assertEquals(-1, compareResult);
+    }
+
+    @Test
+    public void CompareTo_FirstKeysSizeGreater_Returns1() {
+        // Arrange
+        AggregationKey aggregationKey1 = new AggregationKey("a", "b");
+        AggregationKey aggregationKey2 = new AggregationKey("a");
+
+        // Act
+        int compareResult = aggregationKey1.compareTo(aggregationKey2);
+
+        // Assert
+        assertEquals(1, compareResult);
+    }
+
+    @Test
+    public void CompareTo_FirstKeysLowerThanSecond_ReturnsMinus1() {
+        // Arrange
+        AggregationKey aggregationKey1 = new AggregationKey("a", "b");
+        AggregationKey aggregationKey2 = new AggregationKey("a", "c");
+
+        // Act
+        int compareResult = aggregationKey1.compareTo(aggregationKey2);
+
+        // Assert
+        assertEquals(-1, compareResult);
+    }
+
+    @Test
+    public void CompareTo_FirstKeysGreaterThanSecond_Returns1() {
+        // Arrange
+        AggregationKey aggregationKey1 = new AggregationKey("a", "c");
+        AggregationKey aggregationKey2 = new AggregationKey("a", "b");
+
+        // Act
+        int compareResult = aggregationKey1.compareTo(aggregationKey2);
+
+        // Assert
+        assertEquals(1, compareResult);
+    }
+
+
+    //endregion
+
+
+    //region Write and ReadFields tests
+
+    @Test
+    public void WriteReadFields_GivenAggregationKey_BeforeAndAfterAreEqual() throws IOException {
+        // Arrange
+        AggregationKey aggregationKey = new AggregationKey("a", "b");
+
+        // Act
+        AggregationKey outputAggregationKey = callWriteAndRead(aggregationKey);
+
+        // Assert
+        assertEquals(aggregationKey, outputAggregationKey);
+    }
+
+    //endregion
+
+
+    //region Equals tests
+
+    @Test
+    public void Equals_NotEquals_ReturnsFalse() throws IOException {
+        // Arrange
+        AggregationKey aggregationKey1 = new AggregationKey("a", "b");
+        AggregationKey aggregationKey2 = new AggregationKey("a", "c");
+
+        // Act
+        boolean equals = aggregationKey1.equals(aggregationKey2);
+
+        // Assert
+        assertEquals(false, equals);
+    }
+
+    @Test
+    public void Equals_NullReference_ReturnsFalse() throws IOException {
+        // Arrange
+        AggregationKey aggregationKey = new AggregationKey("a", "b");
+
+        // Act
+        boolean equals = aggregationKey.equals(null);
+
+        // Assert
+        assertEquals(false, equals);
+    }
+
+    //endregion
+
+
+    //region Helper methods
+
+    private AggregationKey callWriteAndRead(AggregationKey aggregationKey) throws IOException {
+        AggregationKey outputAggregationKey = new AggregationKey();
+
+        // Prepare output stream classes for serializing
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        DataOutput dataOutput = new DataOutputStream(outputStream);
+
+        // Serialize
+        aggregationKey.write(dataOutput);
+
+        // Prepare input stream classes for deserializing
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+        DataInputStream dataInput = new DataInputStream(inputStream);
+
+        // Deserialize
+        outputAggregationKey.readFields(dataInput);
+
+        return outputAggregationKey;
+    }
+
+    //endregion
+}
